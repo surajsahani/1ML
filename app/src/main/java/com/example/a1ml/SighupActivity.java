33f8dbaa060070201aed5a5f8df8cf1cb14ec503
Suraj sahani <surajkumarsahani1997@gmail.com>
2020-06-22 21:14:38 +0530

    Adding registery files

---
+++ app/src/main/java/com/example/a1ml/SighupActivity.java
@@ -0,0 +1,15 @@
+package com.example.a1ml;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SighupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
+
